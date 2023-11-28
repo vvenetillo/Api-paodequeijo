22000cfc7dd15af596b4a9bd9dabfc6d053a211c
vvenetillo <venetillovictor@gmail.com>
2023-11-27 22:44:49 -0300

    criação da api-paodequeijo

---
+++ index.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-undef */
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Politica de privacidade do cors
+app.use(cors({
+  origin: '*', 
+  methods: 'GET, POST',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
+
+// Leitura em formato Json
+app.use(express.json());
+
+//Importação do model
+let contacto =  {
+  contacts: [
+
+  ]
+}
+
+// rotas 
+  app.get('/contact',  (req, res) => { 
+    console.log(res.body)
+    try{
+    res.json(contacto.contacts);
+    } catch (error){
+      console.error(error);
+      res.status(200).send('Error')
+    }
+  });
+  
+  app.post('/contact', (req, res) => {
+    try {
+      const newContacto = {
+        fname: req.body.fname,
+        femail: req.body.femail,
+        ftelefone: req.body.ftelefone,
+        endereco: {
+          fcep: req.body.fcep,
+          fcidade: req.body.fcidade,
+          fbairro: req.body.fbairro,
+          fmensagem: req.body.fmensagem,
+        },
+      };
+  
+      contacto.contacts.push(newContacto);
+      res.status(201).json(newContacto);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+
+  
+  const dbUdser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASS;
+  const port = process.env.PORT || 3001; 
+
+
+
+  //conexão com o Banco de Dados MongoD
+  mongoose
+    .connect(
+      `mongodb+srv://${dbUdser}:${dbPassword}@cluster0.a7o4it1.mongodb.net/paodequeijo?retryWrites=true&w=majority`
+    )
+    .then(() => {
+      app.listen(port, () => {
+          console.log(`Servidor está rodando na porta ${port}`);
+        });
+    })
+    .catch((err) => console.log(err));
---
+++ models/User.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const mongoose = require('mongoose')
+
+
+// Define o array de objetos 'contacto' com os campos especificados
+const contato = mongoose.model('Contato' ,
+    {
+      
+      fname: String,
+      femail: String,
+      ftelefone: Number,
+      fcidade: String,
+      fmensagem: String,
+      fbairro: String,
+      fcep: Number
+      
+    })
+  
+  // eslint-disable-next-line no-undef  
+  module.exports = contato
